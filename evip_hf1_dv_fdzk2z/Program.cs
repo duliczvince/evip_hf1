b0651cb1eca5b90f2200288f89fa0ac59262d102
duliczvince <55136149+duliczvince@users.noreply.github.com>
2019-09-25 21:08:30 +0200

    Add files via upload

---
+++ evip_hf1_dv_fdzk2z/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evip_hf1_dv_fdzk2z
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("evip hf1 lol");
+            Console.ReadKey();
+        }
+    }
+}
