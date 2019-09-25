2d1cb39beb2409eb081a998b0a546c68432fa529
duliczvince <55136149+duliczvince@users.noreply.github.com>
2019-09-26 01:20:53 +0200

    1_feladat

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace evip_hf1_dv_fdzk2z
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop.RegisterProduct("A", 1);
+            Shop.RegisterProduct("B", 2);
+
+            var price = Shop.GetPrice("AAB");
+            Console.WriteLine(price);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Shop.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace evip_hf1_dv_fdzk2z
+{
+    public class Shop
+    {
+        private static List<Shop> productsList = new List<Shop>();
+        private string product;
+        private int price;
+
+        public string getProduct() { return product; }
+        public int getPrice() { return price; }
+
+        public Shop(string product, int price)
+        {
+            this.product = product;
+            this.price = price;
+        }
+
+        public static void RegisterProduct(string product, int price)
+        {
+            //TODO: vizsgálatok + van-e már, érték > 0 stb.
+
+            productsList.Add(new Shop(product, price));
+        }
+
+        public static int GetPrice(string products)
+        {
+            char[] ProductsOneByOne = products.ToCharArray();
+
+            int price = 0;
+            for (int i = 0; i < ProductsOneByOne.Length; i++)
+            {
+                price += GetPriceForOneProduct(ProductsOneByOne[i]);
+            }
+            return price;
+        }
+
+        private static int GetPriceForOneProduct(char product)
+        {
+            int price = 0;
+            for (int i = 0; i < productsList.Count; i++)
+            {
+                if (productsList[i].getProduct().Equals(product.ToString()))
+                {
+                    price += productsList[i].getPrice();
+                }
+            }
+            return price;
+        }
+
+        
+
+
+    }
+}
