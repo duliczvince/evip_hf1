ad341767d7c120e3b975a71dc277b29b342c59fe
duliczvince <55136149+duliczvince@users.noreply.github.com>
2019-10-03 01:03:04 +0200

    feladat_2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace evip_hf1_dv_fdzk2z
 {
@@ -11,9 +12,10 @@ namespace evip_hf1_dv_fdzk2z
             Shop.RegisterProduct("A", 1);
             Shop.RegisterProduct("B", 2);
 
-            var price = Shop.GetPrice("AAB");
-            Console.WriteLine(price);
+            Shop.RegisterProductWithDiscount("C", 3, 3, 4);
 
+            var price = Shop.GetPrice("ABCCCC"); //12
+            Console.WriteLine(price);
 
             Console.ReadKey();
         }
--- Shop.cs
+++ Shop.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace evip_hf1_dv_fdzk2z
 {
@@ -10,6 +11,9 @@ namespace evip_hf1_dv_fdzk2z
         private string product;
         private int price;
         private int discount;
+        private int discountVAlue;
+
+        #region __1_FELADAT__
 
         public string getProduct() { return product; }
         public int getPrice() { return price; }
@@ -20,11 +24,12 @@ namespace evip_hf1_dv_fdzk2z
             this.price = price;
         }
 
-        public Shop(string product, int price, int discount)
+        public Shop(string product, int price, int discount, int discountVAlue)
         {
             this.product = product;
             this.price = price;
             this.discount = discount;
+            this.discountVAlue = discountVAlue;
         }
 
         public static void RegisterProduct(string product, int price)
@@ -36,34 +41,53 @@ namespace evip_hf1_dv_fdzk2z
 
         public static int GetPrice(string products)
         {
+            //TODO: van-e diszkountos termék? (method) ha van akkor: számolja ki mennyi az ára (method)
+            
+            var result = ShopStringToChar(products).GroupBy(x => x).ToDictionary(y => y.Key, y => y.Count()).OrderByDescending(z => z.Value);
+            int price = 0;
+
+            foreach (var product in result)
+            {
+                Console.WriteLine("Value: " + product.Key + " Count: " + product.Value);
+                var p = new Shop(product.Key, 0);
+                productsList.Find(p);
+            }
+            return price;
+        }
+
+        private static int Find<Shop(this List<Shop> productsList, Shop p)
+        {
+            this.productsList = productsList;
+
+            for(int i = 0; i < productsList.Count; i++)
+            {
+                if(productsList[i].product.Equals(p.product)) { return productsList[i].getPrice(); }
+            }
+            return 0;
+        }
+
+        private static List<string> ShopStringToChar(string products)
+        {
             char[] ProductsOneByOne = products.ToCharArray();
 
-            int price = 0;
+            var productsLocalList = new List<string>();
             for (int i = 0; i < ProductsOneByOne.Length; i++)
             {
-                price += GetPriceForOneProduct(ProductsOneByOne[i]);
+                productsLocalList.Add(ProductsOneByOne[i].ToString());
             }
-            return price;
+
+            return productsLocalList;
         }
 
-        private static int GetPriceForOneProduct(char product)
+        #endregion
+
+        #region __2_FELADAT__
+
+        public static void RegisterProductWithDiscount(string product, int price, int discountFrom, int discountValue)
         {
-            int price = 0;
-            for (int i = 0; i < productsList.Count; i++)
-            {
-                if (productsList[i].getProduct().Equals(product.ToString()))
-                {
-                    price += productsList[i].getPrice();
-                }
-            }
-            return price;
+            productsList.Add(new Shop(product, price, discountFrom, discountValue));
         }
 
-
-
-        public void RegisterCountDiscount(string product, int price, int discount)
-        {
-            productsList.Add(new Shop(product, price, discount));
-        }
+        #endregion
     }
 }
