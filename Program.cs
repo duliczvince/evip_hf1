e84b0aebd489c265b352133c37e7493de42b0d54
duliczvince <55136149+duliczvince@users.noreply.github.com>
2019-10-29 12:29:23 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace evip_hf1_dv_fdzk2z
 {
@@ -11,10 +8,25 @@ namespace evip_hf1_dv_fdzk2z
         {
             Shop.RegisterProduct("A", 1);
             Shop.RegisterProduct("B", 2);
+            Shop.RegisterProduct("B", 1);
+
+            Shop.RegisterProduct("X", 3);
+            Shop.RegisterProduct("Y", 4);
+            Shop.RegisterProduct("Z", 5);
+
+            //újregisztrálás esetén az olcsóbb számít
 
             Shop.RegisterProductWithDiscount("C", 3, 3, 4);
 
-            var price = Shop.GetPrice("ACCCC"); //
+            Shop.RegisterAmountDiscount("D", 10, 5, 0.9);
+
+            Shop.RegisterComboDiscount("ABC", 5, true);
+            Shop.RegisterComboDiscount("XYZ", 11, false);
+
+
+            var price = Shop.GetPrice("tABCXYZ"); //14.4 (5+11) * 0.9
+             price = Shop.GetPrice("AB");
+
             Console.WriteLine(price);
 
             Console.ReadKey();
--- Shop.cs
+++ Shop.cs
@@ -1,95 +1,234 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace evip_hf1_dv_fdzk2z
 {
     public class Shop
     {
+        #region _VARIABLES_
+
         private static List<Shop> productsList = new List<Shop>();
+        private static List<Shop> comboDiscountProductsList = new List<Shop>();
         private string product;
-        private int price;
-        private int discount ;
-        private int discountVAlue ;
+        private double price;
+        private int discount;
+        private int discountValue;
+        private int amount;
+        private double amountValue;
+        private bool onlyForClubMembers;
+        #endregion
 
-        #region __1_FELADAT__
+        private static double RenewRegister(string product, double price)
+        {
+            double oldPrice = price;
+            
+            foreach(Shop v in productsList)
+            {
+                if (v.product.Equals(product))
+                {
+                    oldPrice = v.getPrice();
+                    if (oldPrice > price) {
+                        productsList.Remove(v);
+                        return price; 
+                    }
+                    return oldPrice;
+                }
+            }
 
-        public string getProduct() { return product; }
-        public int getPrice() { return price; }
+            return oldPrice;
+        }
 
-        public Shop(string product, int price)
+        #region _CONSTRUCTORS_
+
+        public Shop(string product, double price)
         {
             this.product = product;
             this.price = price;
         }
 
-        public Shop(string product, int price, int discount, int discountVAlue)
+        public Shop(string product, double price, int discount, int discountValue)
         {
             this.product = product;
             this.price = price;
             this.discount = discount;
-            this.discountVAlue = discountVAlue;
+            this.discountValue = discountValue;
         }
 
-        public static void RegisterProduct(string product, int price)
+        public Shop(string product, double price, int amount, double amountValue)
         {
-            //TODO: vizsgálatok + van-e már, érték > 0 stb.
-
-            productsList.Add(new Shop(product, price));
+            this.product = product;
+            this.price = price;
+            this.amount = amount;
+            this.amountValue = amountValue;
         }
 
-        public static int GetPrice(string products)
+        public Shop(string product, double price, bool onlyForClubMembers)
         {
-            //TODO: van-e diszkountos termék? (method) ha van akkor: számolja ki mennyi az ára (method)
-            
-            var result = ShopStringToChar(products).GroupBy(x => x).ToDictionary(y => y.Key, y => y.Count()).OrderByDescending(z => z.Value);
-            int price = 0;
-
-            foreach (var product in result)
-            {
-                Console.WriteLine("Value: " + product.Key + " Count: " + product.Value);
-                var p = new Shop(product.Key, 0);
-                price += FindElement(p, product.Value);
-            }
-            return price;
+            this.product = product;
+            this.price = price;
+            this.onlyForClubMembers = onlyForClubMembers;
         }
 
-        private static List<string> ShopStringToChar(string products)
-        {
-            char[] ProductsOneByOne = products.ToCharArray();
-
-            var productsLocalList = new List<string>();
-            for (int i = 0; i < ProductsOneByOne.Length; i++)
-            {
-                productsLocalList.Add(ProductsOneByOne[i].ToString());
-            }
-
-            return productsLocalList;
-        }
+        public double getPrice() { return price; }
 
         #endregion
 
-        #region __2_FELADAT__
+        #region _1_FELADAT_
 
-        public static void RegisterProductWithDiscount(string product, int price, int discountFrom, int discountValue)
+        public static void RegisterProduct(string product, double price)
         {
-            productsList.Add(new Shop(product, price, discountFrom, discountValue));
+            ExtensionMethods.CheckRegisteredProductIsValid(product, price);;
+            price = RenewRegister(product, price);
+            productsList.Add(new Shop(product, price));
         }
 
-        private static int FindElement(Shop p, int countProduct)
+        public static double GetPrice(string products)
+        {
+            double clubMemberDiscount = 1;
+            double price = 0;
+            var res = isComboDiscountInCart(ref products, ref price);
+            products = res.Item1;
+            price = res.Item2;
+
+            (string outProduct, double outClubMemberDiscount) = ExtensionMethods.ClubMemberInCart(products, clubMemberDiscount);
+            products = outProduct;
+            clubMemberDiscount = outClubMemberDiscount;
+
+            var result = ExtensionMethods.ShopStringToChar(products).GroupBy(x => x).ToDictionary(y => y.Key, y => y.Count()).OrderByDescending(z => z.Value);
+            foreach (var product in result)
+            {
+                var p = new Shop(product.Key, 0);
+                price += FindElementThenReturnWithItsPrice(p, Convert.ToDouble(product.Value)); //product.Value is int, make it double
+            }
+
+            return price * clubMemberDiscount;
+        }
+
+        private static double FindElementThenReturnWithItsPrice(Shop p, double countProduct)
         {
             foreach (Shop v in productsList)
             {
                 if (v.product.Equals(p.product))
                 {
-                    if (countProduct > 3) { countProduct -= v.discountVAlue - v.discount; }
+                    var discount = GetPriceForDiscountValue(v, countProduct);
+                    if (discount != 0) { return discount; }
+
+                    var amount = GetPriceForAmountValue(v, countProduct);
+                    if (amount != 0) { return amount; }
+
                     return v.getPrice() * countProduct;
                 }
             }
             return 0;
         }
 
+        #endregion
+
+        #region _2_FELADAT_
+
+        public static void RegisterProductWithDiscount(string product, double price, int discountFrom, int discountValue)
+        {
+            productsList.Add(new Shop(product, price, discountFrom, discountValue));
+        }
+
+        private static double GetPriceForDiscountValue(Shop v, double countProduct)
+        {
+            if (countProduct >= v.discountValue && v.discountValue != 0)
+            {
+                countProduct -= v.discountValue - v.discount;
+                return v.getPrice() * countProduct;
+            }
+            return 0;
+        }
+
+        #endregion
+
+        #region _3_FELADAT_
+
+        public static void RegisterAmountDiscount(string products, double price, int amount, double amountValue)
+        {
+            productsList.Add(new Shop(products, price, amount, amountValue));
+        }
+
+        private static double GetPriceForAmountValue(Shop v, double countProduct)
+        {
+            if (countProduct > v.amount && v.amount != 0)
+            {
+                return v.amountValue * v.getPrice() * countProduct;
+            }
+            return 0;
+        }
+
+        #endregion
+
+        #region _4_and_6_FELADAT_
+
+        public static void RegisterComboDiscount(string products, double price, bool onlyForClubMembers)
+        {
+            comboDiscountProductsList.Add(new Shop(products, price, onlyForClubMembers));
+        }
+
+        public static (string, double) isComboDiscountInCart(ref string products, ref double price)
+        {
+            for (int i = 0; i < comboDiscountProductsList.Count; i++)
+            {
+                var firstElementOfCDPL = comboDiscountProductsList[i].product;
+                bool isOnlyForClubMembers = comboDiscountProductsList[i].onlyForClubMembers;
+                bool isClubMemberIncart = ExtensionMethods.isClubMemberIncart(products);
+
+                if (!isClubMemberIncart && isOnlyForClubMembers){ continue; }
+
+                var acronyms = FillAcronymsInList(firstElementOfCDPL, i);
+                var check = CheckThatProductsContainsDiscount(acronyms, products, i);
+                products = check.Item1;
+
+                if (check.Item2 != 0) { price += check.Item2; }
+            }
+            return (products, price);
+        }
+
+        private static List<string> FillAcronymsInList(string firstElementOfCDPL, int i)
+        {
+            firstElementOfCDPL = comboDiscountProductsList[i].product;
+            char[] tmp = firstElementOfCDPL.ToCharArray();
+
+            List<string> acronyms = new List<string>();
+            for (int j = 0; j < tmp.Length; j++)
+            {
+                acronyms.Add(Convert.ToString(tmp[j]));
+            }
+
+            return acronyms;
+        }
+
+        private static (string, double) CheckThatProductsContainsDiscount(List<string> acronyms, string products, int i)
+        {
+            var regex = new Regex(string.Join("|", acronyms), RegexOptions.Compiled);
+            int acronymsCount = acronyms.Count;
+
+            int runs = 0;
+            string productsIsInList = products;
+            for (var match = regex.Match(productsIsInList); match.Success; match = match.NextMatch())
+            {
+                productsIsInList = productsIsInList.Replace(match.Value, "");
+                acronyms.Remove(match.Value);
+                runs++;
+            }
+
+            if (runs == acronymsCount) { return (productsIsInList, comboDiscountProductsList[i].price); }
+
+            return (products, 0);
+        }
+
+        #endregion
+
+        #region _7_FELADAT_
+        public static void UserID()
+        {
+            throw new NotImplementedException();
+        }
         #endregion
     }
 }
